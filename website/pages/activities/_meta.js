d98902e8ff0b32ec4a477c313d76c83ec2791b6f
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-03-11 15:48:30 +0000

    add create project use case

--- website/pages/activities/_meta.js
+++ website/pages/activities/_meta.js
@@ -1,4 +1,6 @@
 export default {
     keycloakAdminAddingNewUser: 'Keycloak New User',
-    submissionManagerAddingNewPerson: 'Submission Layer New User'
+    submissionManagerAddingNewPerson: 'Submission Layer New User',
+    submissionManagerAddingNewProject: 'Submission Layer New Project',
+    TREManagerApprovingProject: 'TRE Project Approval'
 }
