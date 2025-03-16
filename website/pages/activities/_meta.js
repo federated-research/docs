e8dc09b18eca602b1f3561731599da603a33e2a1
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-03-16 21:29:08 +0000

    ro-crate task submission

--- website/pages/activities/_meta.js
+++ website/pages/activities/_meta.js
@@ -2,5 +2,6 @@ export default {
     keycloakAdminAddingNewUser: 'Keycloak New User',
     submissionManagerAddingNewPerson: 'Submission Layer New User',
     submissionManagerAddingNewProject: 'Submission Layer New Project',
-    TREManagerApprovingProject: 'TRE Project Approval'
+    TREManagerApprovingProject: 'TRE Project Approval',
+    submissionUserSubmittingROCrateTask: 'RO-Crate Task Submission'
 }
