5b8b93598a744253af7c38abc1f95438497d11a5
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-04-25 15:49:14 +0100

    reorder index and split out glossary page

--- website/pages/_meta.js
+++ website/pages/_meta.js
@@ -1,5 +1,8 @@
 export default {
   index: "Federated Analytics",
+  acronyms: "Acronyms",
+  actors: "Actors",
+  glossary: "Glossary",
   deployment: "All In One Stack",
   usecases: "Use Cases",
   activities: "Common Activities"
