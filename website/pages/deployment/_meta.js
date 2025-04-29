f76b286d069e6b22380175d8eca99e5524b2981c
vasiliki <vasiliki.panagi@nottingham.ac.uk>
2025-04-29 16:42:03 +0100

    update stack components, add info callouts for stack

--- website/pages/deployment/_meta.js
+++ website/pages/deployment/_meta.js
@@ -1,6 +1,6 @@
 export default {
     quickstart: 'Getting Started',
+    setup: 'Run All In One Demo',
     setup_tes:'Run with Funnel TES',
-    stackinfo: 'Stack Components',
-    setup: 'Run All In One Demo'
+    stackinfo: 'Stack Components'
 }
