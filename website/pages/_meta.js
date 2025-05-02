7cff2bb461d552ef47199e7efbef856e01d26a58
Andy Rae <hello@rae.li>
2025-05-02 14:26:27 +0100

    Add hidden sections for implementation, news, and various topics in _meta.js

--- website/pages/_meta.js
+++ website/pages/_meta.js
@@ -7,5 +7,26 @@ export default {
   first_demo: "All In One (First Demo)",
   deployable_demo: "All In One (Deployable Demo)",
   guides: "Usage Guides",
-  activities: "Common Activities"
+  activities: "Common Activities",
+  implementation: {
+    display: "hidden"
+  },
+  news: {
+    display: "hidden"
+  },
+  "2023-07-11-tre-stakeholder-workshop": {
+    display: "hidden"
+  },
+  "federated-analytics": {
+    display: "hidden"
+  },
+  "public-involvement-and-engagement": {
+    display: "hidden"
+  },
+  "public-trust": {
+    display: "hidden"
+  }
+
+  
+  
 }
