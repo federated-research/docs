054b5856e27b340af861eeae4163a06ec7be7503
Phil Quinlan <philip.quinlan@nottingham.ac.uk>
2025-07-27 21:23:30 +0100

    Changes to start adjusting away from TRE-FX

--- website/pages/_meta.js
+++ website/pages/_meta.js
@@ -1,7 +1,7 @@
 export default {
   index: {
-    title: "TRE-FX",
-    description: "TRE-FX Documentation",
+    title: "Federated Analytics Implementations",
+    description: "Details of the implementations of Trellis weaves in the Health Data Research uK Programme",
     theme: {
       breadcrumb: false,
       layout: 'full',
@@ -12,28 +12,11 @@ export default {
   },
   setup: "Initial Set Up",
   first_demo: "All In One",
-  deploy_stack: "Deploy TRE-FX",
+  deploy_stack: "Deploy Five Safes TES",
   guides: "Usage Guides",
   activities: "Common Activities",
   actors: "Actors",
-  acronyms: "Acronyms",
-  glossary: "Glossary",
   implementation: {
-    display: "hidden"
-  },
-  news: {
-    display: "hidden"
-  },
-  "2023-07-11-tre-stakeholder-workshop": {
-    display: "hidden"
-  },
-  "federated-analytics": {
-    display: "hidden"
-  },
-  "public-involvement-and-engagement": {
-    display: "hidden"
-  },
-  "public-trust": {
     display: "hidden"
   }
 }
