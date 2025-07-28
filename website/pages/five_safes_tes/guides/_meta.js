829918c88d445a87595c411fc5ec6312e379dccc
Phil Quinlan <philip.quinlan@nottingham.ac.uk>
2025-07-28 20:23:53 +0100

    moving into new locations and adding bunny details

--- website/pages/five_safes_tes/guides/_meta.js
+++ website/pages/five_safes_tes/guides/_meta.js
@@ -1,5 +1,11 @@
 export default {
     addresearchertoplatform: 'Add a researcher to the platform',
-    addresearchertoproject: 'Add a researcher to a project',
+    keycloakAdminAddingNewUser: "Adding a new user",
+    submissionManagerAddingNewPerson: "Adding person to Submission layer",
+    submissionManagerAddingNewProject: "Adding new project to Submission layer",
+    TREManagerApprovingProject: "TRE manager approving project",
     createproject: 'Create a new project',
+    addresearchertoproject: 'Add a researcher to a project',
+    EgressManagerApprovingRelease: "Approving a release"
+
 }
