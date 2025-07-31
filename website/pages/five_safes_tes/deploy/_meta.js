c96f670e57e7381258f13b8e2fb6828dfbf399ab
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2025-07-31 15:25:51 +0100

    set tes api page title for nav bar (#39)
    * set tes api page title for nav bar
    
    * fix title

--- website/pages/five_safes_tes/deploy/_meta.js
+++ website/pages/five_safes_tes/deploy/_meta.js
@@ -2,6 +2,7 @@ export default {
     deployment_submission:'Deploy Submission Layer',
     deployment_tre:'Deploy TRE Layer',
     install_funnel:'Install Funnel TES',
+    set_tesapi:'Set TES API agent',
     first_run:'Run your first analysis',
 
 }
