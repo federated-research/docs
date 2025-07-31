8aaace9a2f2296c61b890a1d6ba50067ba24d992
vasiliki <vasiliki.panagi@nottingham.ac.uk>
2025-07-31 15:21:08 +0100

    set tes api page title for nav bar

--- website/pages/five_safes_tes/deploy/_meta.js
+++ website/pages/five_safes_tes/deploy/_meta.js
@@ -2,6 +2,7 @@ export default {
     deployment_submission:'Deploy Submission Layer',
     deployment_tre:'Deploy TRE Layer',
     install_funnel:'Install Funnel TES',
+    set_tesapi:'Set TES API',
     first_run:'Run your first analysis',
 
 }
