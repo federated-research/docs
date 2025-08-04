9d8488f7ffa936b0722b0bee503e8d595b4c15c3
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2025-08-04 14:43:50 +0100

    fix output bucket text & set Download page title (#40)
    * fix submission outputbucket text
    
    * add link on where to raise issues

--- website/pages/five_safes_tes/first_demo/_meta.js
+++ website/pages/five_safes_tes/first_demo/_meta.js
@@ -1,5 +1,7 @@
 export default {
+
     requirements:'Pre-installs required',
+    download:'Download',
     stackinfo:'Core components',
     starting_stack:'Starting the demo',
     funnel_allinone:'Run analysis with Funnel',
