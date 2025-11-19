631fba1102ed04256bf78c861e0ac6aac3fb6034
AndrewThien <anretrithien@gmail.com>
2025-11-19 14:54:26 +0000

    add other elements

--- website/pages/tre_agent/guides/_meta.js
+++ website/pages/tre_agent/guides/_meta.js
@@ -1,5 +1,5 @@
 export default {
-    set_tesapi: "Set TES API",
-    TREManagerApprovingProject: "Approve a project",
-    
-}
+  set_tesapi: "Set TES API",
+  TREManagerApprovingProject: "Approve a project",
+  connecttoegress: "Connect to Egress",
+};
