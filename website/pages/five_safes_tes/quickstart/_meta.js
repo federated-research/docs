3f1986fd1a606af49cc7beef5e58e8d8fa81fbe8
AndrewThien <anretrithien@gmail.com>
2025-12-01 15:03:23 +0000

    clean up

--- website/pages/five_safes_tes/quickstart/_meta.js
+++ website/pages/five_safes_tes/quickstart/_meta.js
@@ -2,7 +2,6 @@ export default {
   requirements: "Requirements",
   download: "Download",
   stackinfo: "Core components",
-  hello_world: "Send the first submission",
-  funnel_allinone: "Run analysis with Funnel",
+  hello_world: "Send the Hello World submission",
   troubleshooting: "Troubleshooting",
 };
