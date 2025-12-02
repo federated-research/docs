628dbe0275b9ba41285b436be6ae4b94376c1160
AndrewThien <anretrithien@gmail.com>
2025-12-02 16:01:24 +0000

    start condensing

--- website/pages/five_safes_tes/quickstart/_meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-  requirements: "Requirements",
-  download: "Download",
-  stackinfo: "Core components",
-  hello_world: "Send Hello World submission",
-  troubleshooting: "Troubleshooting",
-};
