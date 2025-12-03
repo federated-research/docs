da4b840ee46dd5501a60b210844862b46e39ba8b
JonaaEva <jonaa.eva@nottingham.ac.uk>
2025-12-03 15:17:36 +0000

    Updates to Patterns framework, Weave, and Contents structure (#106)
    * doc-content-changes
    
    * grammar edits
    
    * Update website/pages/discovery.mdx
    
    Co-authored-by: Andy Rae <1127507+AndyRae@users.noreply.github.com>
    
    * Update website/pages/discovery.mdx
    
    Co-authored-by: Andy Rae <1127507+AndyRae@users.noreply.github.com>
    
    * Update website/pages/beacon.mdx
    
    Co-authored-by: Jon Couldridge <beforan@jon.gg>
    
    * Update website/pages/discovery/reference_implementation.mdx
    
    Co-authored-by: Andy Rae <1127507+AndyRae@users.noreply.github.com>
    
    * Update website/pages/discovery/reference_implementation.mdx
    
    Co-authored-by: Andy Rae <1127507+AndyRae@users.noreply.github.com>
    
    * Remove 'ethical' from Bunny description
    
    * Update description of Bunny's use cases
    
    Removed 'ethical' from the description of Bunny's capabilities.
    
    ---------
    
    Co-authored-by: Andy Rae <1127507+AndyRae@users.noreply.github.com>
    Co-authored-by: Jon Couldridge <beforan@jon.gg>

--- website/pages/_meta.js
+++ website/pages/_meta.js
@@ -1,6 +1,6 @@
 export default {
   index: {
-    title: "Principles",
+    title: "About us",
     description: "Details of the implementations of Federated Research Patterns and associated weaves in the Health Data Research UK Programme",
     theme: {
       breadcrumb: false,
@@ -10,7 +10,6 @@ export default {
       
     }
   },
-  about_us: "About us",
   standards: "Standards",
   federated_research_patterns: "Federated Research Patterns",
   weaves: {
@@ -18,6 +17,8 @@ export default {
     type: "separator"
   },
   five_safes_tes: "Five Safes TES",
+  discovery: "Discovery",
+  beacon: "Beacon",
   our_software: {
     title: "Software",
     type: "separator"
@@ -25,8 +26,7 @@ export default {
   submission: "Submission Layer",
   tre_agent: "TRE Agent",
   egress: "Egress",
-  bunny: "Bunny",
-  relay: "Relay",
+
   branding_sep: {
     title: "Branding",
     type: "separator"
