c5e25cc727e8165132ad8a89bd88e99569346a20
Tri Thien Nguyen <119530400+AndrewThien@users.noreply.github.com>
2026-03-13 15:50:11 +0000

    Fix revoked docs and tidy up user guides of Submission layer (#160)
    * fix revoked docs
    
    * merge adding researcher to platform
    
    * clean and reorganise menu
    
    * fix links
    
    * clean up
    
    * fix links
    
    * consolidate texts
    
    * change name
    
    * Update website/pages/tre_agent/deploy.mdx
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
    
    * Update website/pages/submission/guides/submissionManagerAddingNewPerson.mdx
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
    
    * fix add user
    
    * TRE admins
    
    * tre DB note
    
    * fix tre db
    
    * Update website/pages/tre_agent/deploy.mdx
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
    
    * Update website/pages/tre_agent/deploy.mdx
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
    
    * Update website/pages/tre_agent/deploy.mdx
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
    
    * Update website/pages/tre_agent/deploy.mdx
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
    
    * fix comment
    
    * fix links
    
    ---------
    
    Co-authored-by: Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>

--- website/pages/submission/guides/_meta.js
+++ website/pages/submission/guides/_meta.js
@@ -1,11 +1,7 @@
 export default {
-    project_setup: 'Project Setup',
-    get_token: 'Get an Access Token',
-    addresearchertoplatform: 'Add a researcher to the platform',
-    keycloakAdminAddingNewUser: "Adding a new user",
-    submissionManagerAddingNewPerson: "Adding person to Submission layer",
-    submissionManagerAddingNewProject: "Adding new project to Submission layer",
-    createproject: 'Create a new project',
-    addresearchertoproject: 'Add a researcher to a project',
-    addtreagent: 'Connect TRE Agent to Submission Layer'
-}
+  submissionManagerAddingNewPerson: "Add a new researcher",
+  submissionManagerSetupNewProject: "Setup a new project",
+  addtreagent: "Connect a TRE Agent",
+  keycloakAdminAddingNewUser: "Add a new user to KeyCloak",
+  get_token: "Get an Access Token",
+};
