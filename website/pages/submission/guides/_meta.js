956039451a4a31ffb8ac5b286fbdd80f989de79c
AndrewThien <anretrithien@gmail.com>
2026-03-13 09:31:34 +0000

    change name

--- website/pages/submission/guides/_meta.js
+++ website/pages/submission/guides/_meta.js
@@ -1,6 +1,6 @@
 export default {
   submissionManagerAddingNewPerson: "Add a new researcher",
-  submissionManagerAddingNewProject: "Add a new project",
+  submissionManagerSetupNewProject: "Setup a new project",
   addtreagent: "Connect a TRE Agent",
   keycloakAdminAddingNewUser: "Add a new user to KeyCloak",
   get_token: "Get an Access Token",
