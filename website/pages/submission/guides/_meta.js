3f3b0dbb23238f87aa36e7a551b30c7bd19319c3
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2026-03-19 13:23:45 +0000

    Add/wizard docs (#164)
    * Add page for TES Wizard and update example
    
    * add cards instead of image
    
    * remove unused image
    
    * Update image and add link to guide
    
    * add info about executor inputs
    
    * list the rest of the inputs from the form

--- website/pages/submission/guides/_meta.js
+++ website/pages/submission/guides/_meta.js
@@ -4,4 +4,5 @@ export default {
   addtreagent: "Connect a TRE Agent",
   keycloakAdminAddingNewUser: "Add a new user to KeyCloak",
   get_token: "Get an Access Token",
+  teswizard: 'Submit a TES task with the TES Wizard'
 };
