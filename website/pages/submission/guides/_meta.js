3114745090f5d701bb5a72b2082c2451051970d5
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2026-07-20 12:03:28 +0100

    Update connecting TRE to Submission to match new Agent Config functionality (#194)
    * add new submission config
    
    * add guide for realm admin
    
    * update guide to add tre agent from submission layer
    
    * update agent deploy page with config and tre connection
    
    * add guide to sidebar
    
    * add connect to submission guide
    
    * add vault to submission layer table and update service stack in tre deploy
    
    * change quickstart to upload agent config
    
    * fix steps
    
    * edit

--- website/pages/submission/guides/_meta.js
+++ website/pages/submission/guides/_meta.js
@@ -1,8 +1,9 @@
 export default {
+  addrealmadmin: "Add a Realm Admin",
+  keycloakAdminAddingNewUser: "Add a new user to KeyCloak",
   submissionManagerAddingNewPerson: "Add a new researcher",
   submissionManagerSetupNewProject: "Setup a new project",
   addtreagent: "Connect a TRE Agent",
-  keycloakAdminAddingNewUser: "Add a new user to KeyCloak",
   get_token: "Get an Access Token",
   teswizard: 'Submit a TES task with the TES Wizard'
 };
