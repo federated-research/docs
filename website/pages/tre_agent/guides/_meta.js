4d12ddbca87e3ac6d7a757ea4277cb8bff422462
AndrewThien <anretrithien@gmail.com>
2026-08-20 16:30:02 +0100

    major clean-up

--- website/pages/tre_agent/guides/_meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-  set_tesapi: "Set TES API",
-  connecttoegress: "Connect to Egress",
-  connecttosubmission: "Connect to Submission Layer",
-  TREManagerApprovingProject: "Approve a project",
-
-};
